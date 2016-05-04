143d6478b4e6c8d920af8bd31ec3391027db499e
Steven Karson <skarson8@gmail.com>
2016-05-04 14:14:49 -0400

    changed main and conditional

--- src/plainsimple/tokitoki/Main.java
+++ src/plainsimple/tokitoki/Main.java
@@ -1,16 +1,10 @@
 package plainsimple.tokitoki;
 
-import java.util.Scanner;
 
 public class Main {
     static Conjugator conj = new Conjugator();
     static Pronoun_Getter pron = new Pronoun_Getter();
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String one = scanner.nextLine();
-            String two = scanner.nextLine();
-            System.out.println(LevenshteinDistance.computeLevenshteinDistance(one, two));
-        }
+        System.out.print(new Verb("sonreir", "conditional", "second", "plural", "null").conjugate());
     }
 }
