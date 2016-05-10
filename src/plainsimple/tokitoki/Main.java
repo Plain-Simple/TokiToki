08ccd8e26de802b1dc2a99711fc562d834cbff42
Steven Karson <skarson8@gmail.com>
2016-05-10 08:33:25 -0400

    Tested tener conditional,doesn't recognize that it's irregular

--- src/plainsimple/tokitoki/Main.java
+++ src/plainsimple/tokitoki/Main.java
@@ -5,6 +5,6 @@ public class Main {
     static Conjugator conj = new Conjugator();
     static Pronoun_Getter pron = new Pronoun_Getter();
     public static void main(String[] args) {
-        System.out.print(new Verb("sonreir", "conditional", "second", "plural", "null").conjugate());
+        System.out.print(new Verb("tener", "conditional", "second", "plural", "null").conjugate());
     }
 }
